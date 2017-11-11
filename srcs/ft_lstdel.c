c365dd12f86bdc661f4f0dfacf69ad17ed2f4b7d
Artem MAKHINYA <amakhiny@e1r2p10.unit.ua>
2017-11-11 12:39:27 +0200

    final_v1

--- srcs/ft_lstdel.c
+++ srcs/ft_lstdel.c
@@ -16,10 +16,13 @@ void	ft_lstdel(t_list **alst, void (*del)(void *, size_t))
 {
 	t_list	*tmp;
 
-	while (*alst != NULL)
+	if (*alst && del)
 	{
-		tmp = (*alst)->next;
-		ft_lstdelone(alst, del);
-		*alst = tmp;
+		while (*alst != NULL)
+		{
+			tmp = (*alst)->next;
+			ft_lstdelone(alst, del);
+			*alst = tmp;
+		}
 	}
 }
--- srcs/ft_lstdelone.c
+++ srcs/ft_lstdelone.c
@@ -14,9 +14,9 @@
 
 void	ft_lstdelone(t_list **alst, void (*del)(void *, size_t))
 {
-	del((*alst)->content, (*alst)->content_size);
-	if (*alst)
+	if (*alst && del)
 	{
+		del((*alst)->content, (*alst)->content_size);
 		free(*alst);
 		*alst = NULL;
 	}
